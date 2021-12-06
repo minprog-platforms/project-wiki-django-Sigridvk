37e7f52eb06a2885e2b1e43341d901adf84147b1
Sigrid <sigridvanklaveren@hotmail.com>
2021-12-06 22:04:05 +0100

    Added error.html and entry.html and entry in views.py

--- encyclopedia/views.py
+++ encyclopedia/views.py
@@ -1,4 +1,5 @@
 from django.shortcuts import render
+import markdown2
 
 from . import util
 
@@ -8,3 +9,13 @@ def index(request):
         "entries": util.list_entries()
     })
 
+def entry(request, title):
+    if any(title.lower() == t.lower() for t in util.list_entries()):
+        return render(request, "encyclopedia/entry.html", {
+        "title": title,
+        "markdown_text": markdown2.markdown(util.get_entry(title))
+    })
+    else:
+        return render(request, "encyclopedia/error.html", {
+            "title": "Error"
+        })
